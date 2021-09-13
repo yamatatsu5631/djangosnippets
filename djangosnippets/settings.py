ecf11f5831a8dea8c87f812e598810caaf75d18b
yama <you@example.com>
2021-09-13 14:24:33 +0000

    plusAdd2

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
