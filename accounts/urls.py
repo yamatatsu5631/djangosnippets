63791635edc5b23740331e5387ab20e475617c2d
yama <you@example.com>
2021-09-14 11:13:35 +0000

    add0914

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     
     path('login/', LoginView.as_view(
         redirect_authenticated_user=True,
-        template_name='accounts/login.html'
+        template_name='accounts/login.html',
     ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
